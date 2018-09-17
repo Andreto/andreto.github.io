d236506c22a004d6a8faa893d79996f89bcb36bf
Andreto <andreas.o.tornkvist@gmail.com>
2018-09-17 17:21:10 +0200

    Andreto Website

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function loadContent() {
-  console.log(content["content-list"].length);
 
   for (i=0;i<content["content-list"].length;i++) {
     var object = content["content"][(content["content-list"][i])]
-    console.log(object["link"]);
 
     var crelmGE = document.createElement("a");crelmGE.className="grid-element";crelmGE.href=object["link"];
     var crelmImg = document.createElement("div");crelmImg.className="ge-img";crelmImg.style=("background-image:url("+object["img"]+");")
@@ -12,4 +10,16 @@ function loadContent() {
 
     document.getElementById("main-grid").appendChild(crelmGE);
   }
+}
+
+function getLatestScratch() {
+  var url = "http://api.spitcast.com/api/spot/forecast/1/";
+  var url_wind = "http://api.spitcast.com/api/county/wind/orange-county/";
+
+  $.when(
+      $.getJSON(url),
+      $.getJSON(url_wind)
+  ).done(function(result1, result2) {
+
+  });
 }
