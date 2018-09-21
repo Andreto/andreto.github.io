b568aba931f5f67520b8f8cc5398a0392215fd4f
Andreto <andreas.o.tornkvist@gmail.com>
2018-09-21 13:37:16 +0200

    custom404 & general

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
 function loadContent() {
 
   for (i=0;i<content["content-list"].length;i++) {
-    var object = content["content"][(content["content-list"][i])]
+    var object = content["content"][(content["content-list"][i])];
 
     var crelmGE = document.createElement("a");crelmGE.className="grid-element";crelmGE.href=object["link"];
-    var crelmImg = document.createElement("div");crelmImg.className="ge-img";crelmImg.style=("background-image:url("+object["img"]+");")
+    var crelmImg = document.createElement("div");crelmImg.className="ge-img";crelmImg.style=("background-image:url("+object["img"]+");");
     var crelmText = document.createElement("div");crelmText.className="ge-text";crelmText.innerHTML=object["title"];
     crelmGE.appendChild(crelmImg);crelmGE.appendChild(crelmText);
 
