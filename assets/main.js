e79b97f754fff23f2e2726a4caee40567dbabda3
Andreto <andreas.o.tornkvist@gmail.com>
2019-09-24 18:54:48 +0200

    nav style

--- assets/main.js
+++ assets/main.js
@@ -30,3 +30,14 @@ function togleContent(n) {
   $("#cont-switch-indicator-" + ["prjct", "prtfl"][n]).removeClass("inactive");
   $("#cont-switch-indicator-" + ["prtfl", "prjct"][n]).addClass("inactive");
 }
+
+function pageScroll() {
+  var elems = document.querySelectorAll(".nav-fill");
+  for (var i = 0; i < elems.length; i++) {
+    if (window.scrollY == 0){
+      elems[i].style.boxShadow = "none"
+    }else{
+      elems[i].style.boxShadow = "0px 4px 8px -4px rgba(0,0,0,0.4)"
+    }
+  }
+}
