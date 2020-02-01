26eb478fb59aba67403afa9ce9ee7dcddf22a1cd
Andreas Törnkvist <andreas.o.tornkvist@gmail.com>
2020-02-01 17:15:34 +0100

    customHighlight page add.

--- assets/content.js
+++ assets/content.js
@@ -21,7 +21,7 @@ var content = {
       img: "https://andreto.github.io/assets/thumbnails/CustomHighlight.png",
       title: "Custom Highlight",
       type: "Chrome Extension",
-      link:"https://andreto.github.io/CustomHighlight/"
+      link:"https://andreto.github.io/p/CustomHighlight/"
     },
     gobocf: {
       img: "https://andreto.github.io/assets/thumbnails/gobocf.png",
